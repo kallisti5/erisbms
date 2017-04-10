4a7f4c15cf39944446acd20ea2209c36e1a81737
Alexander von Gluck IV <kallisti5@unixzen.com>
2017-04-10 07:40:03 -0500

    monitoring: Begin monitoring cell thresholds

--- src/main.ino
+++ src/main.ino
@@ -8,12 +8,17 @@
 
 #define STATUS_PIN 13
 
+#define V_MIN 3.0
+#define V_MAX 4.2
+
 ////////////////////////
 /////// Pin Assignments
 ////////////////////////
 //
 // DumpPins and MonitorPins should always have
 // the same number of elemants!
+//
+// CELL_COUNT = nS (1S, 2S, 3S, etc)
 //
 #define CELL_COUNT 3
 //
@@ -29,11 +34,19 @@ int MonitorPins[] = {
 //
 /////////
 
+uint8_t DumpState[CELL_COUNT] = {LOW};
+
 void setup() {
   #ifdef STATUS_PIN
   pinMode(STATUS_PIN, OUTPUT);
   digitalWrite(STATUS_PIN, LOW);
   #endif
+
+  Serial.begin(9600);
+  Serial.println("I: ErisBMS - Smartly control chaos");
+  Serial.print("I: Version ");
+  Serial.print(VERSION);
+  Serial.print("\n");
 
   // Set relay dump pins to output
   for (int i = 0; i < CELL_COUNT; i++) {
@@ -41,6 +54,9 @@ void setup() {
   }
   // Set monitor pins to input
   for (int i = 0; i < CELL_COUNT; i++) {
+    Serial.print("I: Setup Cell ");
+    Serial.print(i);
+    Serial.print("\n");
     pinMode(MonitorPins[i], INPUT);
   }
 }
@@ -53,10 +69,41 @@ void status_flick(int time) {
   #endif
 }
 
-void loop() {
+void update_dumpstates()
+{
   for (int i = 0; i < CELL_COUNT; i++) {
-    status_flick(10);
-    int v = analogRead(MonitorPins[i]);
-    delay(100);
+    if (DumpState[i] == HIGH) {
+      Serial.print("A:Shunt active on cell ");
+      Serial.print(i);
+      Serial.print("\n");
+    }
+    digitalWrite(DumpPins[i], DumpState[i]);
   }
+}
+
+void loop() {
+  float minimum = V_MAX;
+
+  for (int i = 0; i < CELL_COUNT; i++) {
+    status_flick(5);
+    float v = analogRead(MonitorPins[i]) * (5.0 / 1023.0);
+	minimum = min(minimum, v);
+    Serial.print("D:");
+    Serial.print(i);
+    Serial.print(":");
+    Serial.print(v);
+    Serial.print("\n");
+    if (v > V_MAX) {
+      DumpState[i] = HIGH;
+    } else {
+      DumpState[i] = LOW;
+    }
+  }
+  if (minimum < V_MIN) {
+    Serial.print("A:Cell depleted @");
+    Serial.print(minimum);
+    Serial.print("\n");
+  }
+  delay(500);
+  update_dumpstates();
 }
