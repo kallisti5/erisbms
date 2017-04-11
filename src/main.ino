b543881e5bd823612fd6d44968783bbe6c54bf88
Alexander von Gluck IV <kallisti5@unixzen.com>
2017-04-11 15:48:43 -0500

    main: Implement maximum cell range.
    * Battery pack cell outside of maximum
      voltage ranges results in main relay
      disconnect.

--- src/main.ino
+++ src/main.ino
@@ -6,41 +6,51 @@
  *
  */
 
+// Status LED (optional)
 #define STATUS_PIN 13
 
-#define V_MIN 3.0
-#define V_MAX 4.2
+// Battery Pack Voltage (main relay / input monitoring)
+#define BPV_RELAY_PIN 12
+#define BPV_MONITOR_PIN A5
 
-////////////////////////
-/////// Pin Assignments
-////////////////////////
-//
-// DumpPins and MonitorPins should always have
-// the same number of elemants!
-//
-// CELL_COUNT = nS (1S, 2S, 3S, etc)
-//
+// Cell Conditions
+//  Minimum acceptable cell voltage before BPV cut-off
+#define V_MIN 3.0
+//  Maximum acceptable cell voltage before dump resistor
+#define V_MAX 4.15
+//  Maximum acceptable cell voltage before BPV cut-off
+#define V_CRITICAL 4.21
+
+// Pin Assignments
+//   Number of series cell groups  (1S, 2S, etc)
 #define CELL_COUNT 3
 //
-// Digital pins with dump relays
+// Cell pin assignments, number of entries should match CELL_COUNT
+//
+//   Digital pins with dump relays
 int DumpPins[] = {
  2, 3, 4
 };
 //
-// Analog pins monitoring cell voltages
+//   Analog pins monitoring cell voltages
 int MonitorPins[] = {
  A0, A1, A2
 };
-//
-/////////
 
 uint8_t DumpState[CELL_COUNT] = {LOW};
+uint8_t BPVRelay = LOW;
 
-void setup() {
+void setup()
+{
   #ifdef STATUS_PIN
   pinMode(STATUS_PIN, OUTPUT);
   digitalWrite(STATUS_PIN, LOW);
   #endif
+
+  // Setup and disable main BPV relay until we
+  // check our batteries.
+  pinMode(BPV_RELAY_PIN, OUTPUT);
+  digitalWrite(BPV_RELAY_PIN, LOW);
 
   Serial.begin(9600);
   Serial.println("I: ErisBMS - Smartly control chaos");
@@ -61,7 +71,8 @@ void setup() {
   }
 }
 
-void status_flick(int time) {
+void status_flick(int time)
+{
   #ifdef STATUS_PIN
   digitalWrite(STATUS_PIN, HIGH);
   delay(time);
@@ -69,7 +80,7 @@ void status_flick(int time) {
   #endif
 }
 
-void update_dumpstates()
+void update_dumprelay_pins()
 {
   for (int i = 0; i < CELL_COUNT; i++) {
     if (DumpState[i] == HIGH) {
@@ -81,29 +92,50 @@ void update_dumpstates()
   }
 }
 
-void loop() {
+void update_bpvrelay_pin()
+{
+  // BPV Relay is N.O.
+
+  if (BPVRelay == HIGH)
+    Serial.print("I:Battery Pack Online\n");
+  else
+    Serial.print("I:Battery Pack Offline\n");
+
+  digitalWrite(BPV_RELAY_PIN, BPVRelay);
+}
+
+void loop()
+{
   float minimum = V_MAX;
+  float maximum = V_MAX;
 
   for (int i = 0; i < CELL_COUNT; i++) {
     status_flick(5);
     float v = analogRead(MonitorPins[i]) * (5.0 / 1023.0);
-	minimum = min(minimum, v);
+
+    minimum = min(minimum, v);
+    maximum = max(maximum, v);
+
     Serial.print("D:");
     Serial.print(i);
     Serial.print(":");
     Serial.print(v);
     Serial.print("\n");
+
     if (v > V_MAX) {
       DumpState[i] = HIGH;
     } else {
       DumpState[i] = LOW;
     }
   }
-  if (minimum < V_MIN) {
-    Serial.print("A:Cell depleted @");
-    Serial.print(minimum);
-    Serial.print("\n");
-  }
+
+  if (maximum > V_CRITICAL || minimum < V_MIN)
+    BPVRelay = LOW;
+  else
+    BPVRelay = HIGH;
+
+  update_bpvrelay_pin();
+  update_dumprelay_pins();
+
   delay(500);
-  update_dumpstates();
 }
